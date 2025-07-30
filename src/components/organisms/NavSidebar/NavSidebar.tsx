aa76fc23357666f919237cd6e9c8a8cb5f019be7
ThejanNim <thejannims@gmail.com>
2025-07-30 07:40:33 +0530

    feat: implement Navigation sidebar and add initial routes.

---
+++ src/components/organisms/NavSidebar/NavSidebar.tsx
@@ -0,0 +1,144 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "../../atoms/Sidebar/Sidebar";
+import { NavMenu } from "./NavMenu/NavMenu";
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  GalleryVerticalEnd,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react";
+
+export default function NavSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  const data = {
+    user: {
+      name: "shadcn",
+      email: "m@example.com",
+      avatar: "/avatars/shadcn.jpg",
+    },
+    teams: [
+      {
+        name: "Acme Inc",
+        logo: GalleryVerticalEnd,
+        plan: "Enterprise",
+      },
+      {
+        name: "Acme Corp.",
+        logo: AudioWaveform,
+        plan: "Startup",
+      },
+      {
+        name: "Evil Corp.",
+        logo: Command,
+        plan: "Free",
+      },
+    ],
+    navMain: [
+      {
+        title: "Tracker",
+        url: "#",
+        icon: SquareTerminal,
+        isActive: true,
+        items: [
+          {
+            title: "Jobs",
+            url: "/job-tracker",
+          },
+          {
+            title: "People",
+            url: "/people",
+          },
+          {
+            title: "Companies",
+            url: "/companies",
+          },
+        ],
+      },
+      {
+        title: "Models",
+        url: "#",
+        icon: Bot,
+        items: [
+          {
+            title: "Genesis",
+            url: "#",
+          },
+          {
+            title: "Explorer",
+            url: "#",
+          },
+          {
+            title: "Quantum",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Documentation",
+        url: "#",
+        icon: BookOpen,
+        items: [
+          {
+            title: "Introduction",
+            url: "#",
+          },
+          {
+            title: "Get Started",
+            url: "#",
+          },
+          {
+            title: "Tutorials",
+            url: "#",
+          },
+          {
+            title: "Changelog",
+            url: "#",
+          },
+        ],
+      },
+      {
+        title: "Settings",
+        url: "#",
+        icon: Settings2,
+        items: [
+          {
+            title: "General",
+            url: "#",
+          },
+          {
+            title: "Team",
+            url: "#",
+          },
+          {
+            title: "Billing",
+            url: "#",
+          },
+          {
+            title: "Limits",
+            url: "#",
+          },
+        ],
+      },
+    ],
+  };
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>{/* <TeamSwitcher teams={data.teams} /> */}</SidebarHeader>
+      <SidebarContent>
+        <NavMenu items={data.navMain} />
+        {/* <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
