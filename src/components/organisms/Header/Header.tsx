252f5f5664c37e9610752fa7e6e330455f0e6519
ThejanNim <thejannims@gmail.com>
2025-08-05 06:49:40 +0530

    feat: install tanstack table and implement job tracking table

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="h-[48px] border-b border-[BBBBBB] flex items-center font-medium px-4 text-[20px]">
+            <div>Jobs</div>
+        </header>
+    )
+}
--- src/components/templates/AppTemplate.tsx
+++ src/components/templates/AppTemplate.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router";
 import NavSidebar from "../organisms/NavSidebar/NavSidebar";
 import { SidebarProvider } from "../atoms/Sidebar/Sidebar";
+import Header from "../organisms/Header/Header";
 
 export default function AppTemplate() {
   return (
     <SidebarProvider>
-      <div className="flex">
+      <div className="flex w-full">
         <NavSidebar />
-        <main>
+        <main className="w-full">
+          <Header />
           <Outlet />
         </main>
       </div>
